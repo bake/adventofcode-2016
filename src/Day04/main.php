cfb2bb96d2f01f713d556b625f2bd0e275b151d8
bake <bake@192k.pw>
2022-01-12 19:34:05 +0100

    Add day 4

---
+++ src/Day04/main.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace Bake\AdventOfCode2015\Day04;
+
+class Room
+{
+  public function __construct(
+    public readonly string $name,
+    public readonly int $id,
+    public readonly string $checksum,
+  ) {
+  }
+
+  public function validate(): bool
+  {
+    $chars = [];
+    $name = $this->name;
+    $name = str_replace('-', '', $name);
+    $name = str_split($name);
+    foreach (array_count_values($name) as $char => $num) {
+      $chars[$num][] = $char;
+    }
+    krsort($chars);
+    $chars = array_values($chars);
+
+    $checksum = str_split($this->checksum);
+    while ($char = array_shift($checksum)) {
+      if (!in_array($char, $chars[0])) return false;
+      $chars[0] = array_diff($chars[0], [$char]);
+      if (empty($chars[0])) unset($chars[0]);
+      $chars = array_values($chars);
+    }
+    return true;
+  }
+
+  public function decrypt(): string
+  {
+    $name = str_split($this->name);
+    $name = array_map(function (string $char): string {
+      if ($char === '-') return ' ';
+      return chr((ord($char) - ord('a') + $this->id) % 26 + ord('a'));
+    }, $name);
+    return implode($name);
+  }
+}
+
+function input($handle): iterable
+{
+  while ($room = fgets($handle)) {
+    $name = explode('-', trim($room, "\n]"));
+    $id = array_pop($name);
+    $name = implode('-', $name);
+    [$id, $checksum] = explode('[', $id);
+    yield new Room($name, (int) $id, $checksum);
+  }
+}
+
+function part1(array $rooms): int
+{
+  $rooms = array_filter($rooms, fn (Room $room): bool => $room->validate());
+  return array_sum(array_map(fn (Room $room): int => $room->id, $rooms));
+}
+
+function part2(array $rooms): int
+{
+  $rooms = array_filter($rooms, fn (Room $room): bool => $room->validate());
+  $rooms = array_filter($rooms, fn (Room $room): bool => $room->decrypt() === 'northpole object storage');
+  return empty($rooms) ? 0 : reset($rooms)->id;
+}
---
+++ src/Day04/main_test.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Bake\AdventOfCode2015\Day04;
+
+require __DIR__ . '/main.php';
+
+test('day 4 validate aaaaa-bbb-z-y-x-123[abxyz]', function (): void {
+  $room = new Room('aaaaa-bbb-z-y-x', 123, 'abxyz');
+  expect($room->validate())->toBeTrue();
+})->group('day04', 'sample');
+
+test('day 4 validate a-b-c-d-e-f-g-h-987[abcde]', function (): void {
+  $room = new Room('a-b-c-d-e-f-g-h', 987, 'abcde');
+  expect($room->validate())->toBeTrue();
+})->group('day04', 'sample');
+
+test('day 4 validate not-a-real-room-404[oarel]', function (): void {
+  $room = new Room('not-a-real-room', 404, 'oarel');
+  expect($room->validate())->toBeTrue();
+})->group('day04', 'sample');
+
+test('day 4 validate totally-real-room-200[decoy]', function (): void {
+  $room = new Room('totally-real-room', 200, 'decoy');
+  expect($room->validate())->toBeFalse();
+})->group('day04', 'sample');
+
+test('day 4 decrypt qzmt-zixmtkozy-ivhz-343', function (): void {
+  $room = new Room('qzmt-zixmtkozy-ivhz', 343, '');
+  expect($room->decrypt())->toBe('very encrypted name');
+})->group('day04', 'sample');
+
+test('day 4 input', function (): void {
+  $handle = fopen(__DIR__ . '/input.txt', 'r');
+  $rooms = [...input($handle)];
+  expect(part1($rooms))->toBe(245102);
+  expect(part2($rooms))->toBe(324);
+})->group('day04', 'input');
