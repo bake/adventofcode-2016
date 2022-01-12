a2a58c931a712073ff90fca499bd89ad5e51dd44
bake <bake@192k.pw>
2022-01-12 19:33:56 +0100

    Add day 1

---
+++ src/Day01/main.php
@@ -0,0 +1,143 @@
+<?php
+
+namespace Bake\AdventOfCode2015\Day01;
+
+enum Direction: string
+{
+  case Left = 'L';
+  case Right = 'R';
+
+  public function int(): int
+  {
+    return match ($this) {
+      Direction::Left => -1,
+      Direction::Right => 1,
+    };
+  }
+}
+
+class Instruction
+{
+  public function __construct(
+    public readonly Direction $direction,
+    public readonly int $distance,
+  ) {
+  }
+
+  public static function from(string $string): self
+  {
+    return new self(Direction::from($string[0]), substr($string, 1));
+  }
+}
+
+class Point
+{
+  public function __construct(
+    public readonly int $x,
+    public readonly int $y,
+  ) {
+  }
+
+  public function add(Point $p): self
+  {
+    return new self(
+      $this->x + $p->x,
+      $this->y + $p->y,
+    );
+  }
+
+  public function __toString(): string
+  {
+    return "{$this->x},{$this->y}";
+  }
+}
+
+class Entity
+{
+  public function __construct(
+    public readonly Point $position,
+    public readonly int $direction,
+  ) {
+  }
+
+  public static function zero(): self
+  {
+    return new self(
+      new Point(0, 0),
+      0,
+    );
+  }
+
+  public function instruction(Instruction $instruction): self
+  {
+    $entity = $this;
+    $entity = $entity->rotate($instruction->direction);
+    $entity = $entity->move($instruction->distance);
+    return $entity;
+  }
+
+  public function rotate(Direction $direction): self
+  {
+    return new self(
+      $this->position,
+      ($this->direction + $direction->int() + 4) % 4,
+    );
+  }
+
+  public function move(int $distance): self
+  {
+    return new self(
+      $this->position->add(new Point(
+        match ($this->direction) {
+          1 => $distance,
+          3 => -$distance,
+          default => 0,
+        },
+        match ($this->direction) {
+          0 => -$distance,
+          2 => $distance,
+          default => 0,
+        },
+      )),
+      $this->direction,
+    );
+  }
+}
+
+function input($handle): array
+{
+  $instructions = explode(',', trim(stream_get_contents($handle)));
+  $instructions = array_map(trim(...), $instructions);
+  $instructions = array_map(Instruction::from(...), $instructions);
+  return $instructions;
+}
+
+function part1(array $instructions): int
+{
+  $entity = array_reduce(
+    $instructions,
+    fn (Entity $e, Instruction $i): Entity => $e->instruction($i),
+    Entity::zero(),
+  );
+  return abs($entity->position->x) + abs($entity->position->y);
+}
+
+function part2(array $instructions): int
+{
+  $entity = Entity::zero();
+  $visited = [];
+  foreach ($instructions as $instruction) {
+    $prev = $entity->position;
+    $entity = $entity->instruction($instruction);
+    $curr = $entity->position;
+    while ($curr->x !== $prev->x || $curr->y !== $prev->y) {
+      $prev = $prev->add(new Point(
+        $curr->x <=> $prev->x,
+        $curr->y <=> $prev->y,
+      ));
+      if (isset($visited["{$prev}"])) return $visited["{$prev}"];
+      $visited["{$prev}"] = abs($prev->x) + abs($prev->y);
+    }
+  }
+  return 0;
+}
---
+++ src/Day01/main_test.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Bake\AdventOfCode2015\Day01;
+
+require __DIR__ . '/main.php';
+
+test('day 1 part 1 R2, L3', function (): void {
+  $handle = string_to_stream(<<<PLAIN
+  R2, L3
+  PLAIN);
+  expect(part1(input($handle)))->toBe(5);
+})->group('day01', 'sample');
+
+test('day 1 part 1 R2, R2, R2', function (): void {
+  $handle = string_to_stream(<<<PLAIN
+  R2, R2, R2
+  PLAIN);
+  expect(part1(input($handle)))->toBe(2);
+})->group('day01', 'sample');
+
+test('day 1 part 1 R5, L5, R5, R3', function (): void {
+  $handle = string_to_stream(<<<PLAIN
+  R5, L5, R5, R3
+  PLAIN);
+  expect(part1(input($handle)))->toBe(12);
+})->group('day01', 'sample');
+
+test('day 1 part 2 R8, R4, R4, R8', function (): void {
+  $handle = string_to_stream(<<<PLAIN
+  R8, R4, R4, R8
+  PLAIN);
+  expect(part2(input($handle)))->toBe(4);
+})->group('day01', 'sample');
+
+test('day 1 input', function (): void {
+  $handle = fopen(__DIR__ . '/input.txt', 'r');
+  $instructions = input($handle);
+  expect(part1($instructions))->toBe(181);
+  expect(part2($instructions))->toBe(140);
+})->group('day01', 'input');
