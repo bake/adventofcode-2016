b5a2644f4774d7c504e9d1aae0198201cc43613a
bake <bake@192k.pw>
2022-01-13 09:44:49 +0100

    Add day 6

---
+++ src/Day06/main.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Bake\AdventOfCode2015\Day06;
+
+function input($handle): array
+{
+  $messages = explode(PHP_EOL, stream_get_contents($handle));
+  $messages = array_filter($messages);
+  $messages = array_map(str_split(...), $messages);
+  return $messages;
+}
+
+function part1(array $messages): string
+{
+  $message = '';
+  foreach (array_keys(reset($messages)) as $i) {
+    $column = array_column($messages, $i);
+    $column = array_count_values($column);
+    arsort($column);
+    $message .= key($column);
+  }
+  return $message;
+}
+
+function part2(array $messages): string
+{
+  $message = '';
+  foreach (array_keys(reset($messages)) as $i) {
+    $column = array_column($messages, $i);
+    $column = array_count_values($column);
+    asort($column);
+    $message .= key($column);
+  }
+  return $message;
+}
---
+++ src/Day06/main_test.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Bake\AdventOfCode2015\Day06;
+
+require __DIR__ . '/main.php';
+
+test('day 6 sample', function (): void {
+  $handle = string_to_stream(<<<PLAIN
+  eedadn
+  drvtee
+  eandsr
+  raavrd
+  atevrs
+  tsrnev
+  sdttsa
+  rasrtv
+  nssdts
+  ntnada
+  svetve
+  tesnvt
+  vntsnd
+  vrdear
+  dvrsen
+  enarar
+  PLAIN);
+  $messages = input($handle);
+  expect(part1($messages))->toBe('easter');
+  expect(part2($messages))->toBe('advent');
+})->group('day06', 'sample');
+
+test('day 6 input', function (): void {
+  $handle = fopen(__DIR__ . '/input.txt', 'r');
+  $messages = [...input($handle)];
+  expect(part1($messages))->toBe('qoclwvah');
+  expect(part2($messages))->toBe('ryrgviuv');
+})->group('day06', 'input');
