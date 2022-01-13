7f08fd09dd9f21d1afd769348de490ef724731e0
bake <bake@192k.pw>
2022-01-13 09:44:52 +0100

    Add day 7

---
+++ src/Day07/main.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace Bake\AdventOfCode2015\Day07;
+
+function input($handle): iterable
+{
+  while ($ip = fgets($handle)) {
+    yield IPv7::from($ip);
+  }
+}
+
+class IPv7
+{
+  public function __construct(
+    public readonly array $even,
+    public readonly array $odd,
+  ) {
+  }
+
+  public static function from(string $ip): self
+  {
+    $parts = str_replace('[', '|', $ip);
+    $parts = str_replace(']', '|', $parts);
+    $parts = explode('|', $parts);
+    return new self(
+      array_filter(
+        $parts,
+        fn (int $key): bool => $key % 2 === 0,
+        ARRAY_FILTER_USE_KEY,
+      ),
+      array_filter(
+        $parts,
+        fn (int $key): bool => $key % 2 === 1,
+        ARRAY_FILTER_USE_KEY,
+      ),
+    );
+  }
+
+  public function tls(): bool
+  {
+    $contains_abba = function (string $str): bool {
+      for ($i = 0; $i <= strlen($str) - 4; $i++) {
+        if ($str[$i + 0] === $str[$i + 1]) continue;
+        if ($str[$i + 0] !== $str[$i + 3]) continue;
+        if ($str[$i + 1] !== $str[$i + 2]) continue;
+        return true;
+      }
+      return false;
+    };
+    $even = array_filter($this->even, $contains_abba);
+    $odd = array_filter($this->odd, $contains_abba);
+    return !empty($even) && empty($odd);
+  }
+
+  public function ssl(): bool
+  {
+    $abas = function (string $str): iterable {
+      $abas = [];
+      for ($i = 0; $i <= strlen($str) - 3; $i++) {
+        if ($str[$i + 0] === $str[$i + 1]) continue;
+        if ($str[$i + 0] !== $str[$i + 2]) continue;
+        $abas[] = substr($str, $i, 3);
+      }
+      return $abas;
+    };
+    $even = array_merge(...array_map($abas, $this->even));
+    $odd = array_merge(...array_map($abas, $this->odd));
+    foreach ($even as $aba) {
+      $bab = "{$aba[1]}{$aba[0]}{$aba[1]}";
+      if (in_array($bab, $odd)) return true;
+    }
+    return false;
+  }
+}
+
+function part1(array $ips): int
+{
+  return count(array_filter($ips, fn (IPv7 $ip): bool => $ip->tls()));
+}
+
+function part2(array $ips): int
+{
+  return count(array_filter($ips, fn (IPv7 $ip): bool => $ip->ssl()));
+}
---
+++ src/Day07/main_test.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Bake\AdventOfCode2015\Day07;
+
+require __DIR__ . '/main.php';
+
+test('day 7 part 1 sample', function (): void {
+  $handle = string_to_stream(<<<PLAIN
+  abba[mnop]qrst
+  abcd[bddb]xyyx
+  aaaa[qwer]tyui
+  ioxxoj[asdfgh]zxcvbn
+  PLAIN);
+  $ips = [...input($handle)];
+  expect(part1($ips))->toBe(2);
+})->group('day07', 'sample');
+
+test('day 7 part 2 sample', function (): void {
+  $handle = string_to_stream(<<<PLAIN
+  aba[bab]xyz supports
+  xyx[xyx]xyx
+  aaa[kek]eke
+  zazbz[bzb]cdb
+  PLAIN);
+  $ips = [...input($handle)];
+  expect(part2($ips))->toBe(3);
+})->group('day07', 'sample');
+
+test('day 7 input', function (): void {
+  $handle = fopen(__DIR__ . '/input.txt', 'r');
+  $ips = [...input($handle)];
+  expect(part1($ips))->toBe(105);
+  expect(part2($ips))->toBe(258);
+})->group('day07', 'input');
